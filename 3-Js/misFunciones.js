4e4dcfd805e107457a1e880ba4e680a39eca219f
agostinasolla <2502416@ucc.edu.ar>
2026-09-21 08:12:03 -0300

    conversor de unidades

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -5,3 +5,32 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirunidades(id, valor){
+    if (isNaN(valor)){
+        alert("Se ingreso un valor incorrecto: " + id);
+        document.getElementById("pulgada").value = " "; 
+        document.getElementById("pie").value = " ";
+        document.getElementById("yarda").value = " ";
+    }
+    else if (id === "metro"){
+        document.getElementById("pulgada").value = valor*39; 
+        document.getElementById("pie").value = valor*3.2804;
+        document.getElementById("yarda").value = valor*1.90361;
+    }
+    else if (id == "pulgada"){
+        document.getElementById("metro").value = valor*0.0254;
+        document.getElementById("pie").value = valor*0.08333;
+        document.getElementById("yarda").value = valor*0.0277;
+    }
+    else if (id == "pie"){
+        document.getElementById("pulgada").value = valor*12;
+        document.getElementById("metro").value = valor*0.304;
+        document.getElementById("yarda").value = valor*0.333;
+    }
+    else if (id == "yarda"){
+        document.getElementById("pulgada").value = valor*36;
+        document.getElementById("metro").value = valor*0.914;
+        document.getElementById("pie").value = valor*3;
+    }
+}
--- Readme.md
+++ Readme.md
@@ -51,8 +51,8 @@
 
 ## Unidad 3: JavaScript y ES6+
 
-- [ ] Hola Mundo!
-- [ ] Conversor de Unidades
+- [x] Hola Mundo!
+- [x] Conversor de Unidades
 - [ ] Documentación
 - [ ] Grados a Radianes
 - [ ] Refactorización
